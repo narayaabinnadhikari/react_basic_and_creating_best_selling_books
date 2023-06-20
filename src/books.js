5f5b069af0ee53a46e57a6a65d2bf1f8c6a7421b
narayaabinnadhkari <narayanabin223@gmail.com>
2023-06-20 21:10:48 +0545

    many commits

---
+++ src/books.js
@@ -0,0 +1,31 @@
+import img1 from './images/DrSeusOhtheplaces.jpg'
+import img2 from './images/Atomic habits.jpg'
+import img3 from './images/lesson_in_chemistry.jpg'
+import img4 from "./images/DadJokes.jpg"
+
+export const books = [
+  {
+    author: "Dr. Seuss",
+    title: `Oh, the Places You'll Go!`,
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: `Atomic Habits`,
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Bonnie Garmus",
+    title: `Lessons in Chemistry`,
+    img: img3,
+    id: 3,
+  },
+  {
+    author: "Jimmy Niro",
+    title: `Dad Jokes`,
+    img: img4,
+    id: 4,
+  },
+];
